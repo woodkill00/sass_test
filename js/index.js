9a3385da54d72e985099377f28cdd3527501b59b
mdalamin07 <mdalamin67378@gmail.com>
2022-07-20 23:06:17 +0600

    creat a landing page

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
 /******* Dropdown Start Here *******/ 
 
-$(".about_dropdown").click(function () {
+$(".about_dropdown").hover(function () {
     $(".about_dropdown_icon").toggleClass("down");
 })
-$(".focus_dropdown").click(function () {
+$(".focus_dropdown").hover(function () {
     $(".focus_dropdown_icon").toggleClass("down");
 })
-$(".shop_dropdown").click(function () {
+$(".shop_dropdown").hover(function () {
     $(".shop_dropdown_icon").toggleClass("down");
 })
 
 /******* Dropdown End Here *******/ 
+
+const hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener('click', ()=>{
+   //Animate Links
+    // navLinks.classList.toggle("open");
+    // links.forEach(link => {
+    //     link.classList.toggle("fade");
+    // });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
