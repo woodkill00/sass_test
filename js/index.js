30f5e1e2d5a7ac9c34816d9ad513dbfd5eb7daa1
woodkill00 <thejunkmailvoid@gmail.com>
2022-07-27 20:57:06 -0500

    update to landing page

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// /******* Dropdown Start Here *******/ 
+
+// $(".about_dropdown").hover(function () {
+//     $(".about_dropdown_icon").toggleClass("down");
+// })
+// $(".focus_dropdown").hover(function () {
+//     $(".focus_dropdown_icon").toggleClass("down");
+// })
+// $(".shop_dropdown").hover(function () {
+//     $(".shop_dropdown_icon").toggleClass("down");
+// })
+
+// /******* Dropdown End Here *******/ 
+
+// const hamburger = document.querySelector(".hamburger");
+// hamburger.addEventListener('click', ()=>{
+//    //Animate Links
+//     // navLinks.classList.toggle("open");
+//     // links.forEach(link => {
+//     //     link.classList.toggle("fade");
+//     // });
+
+//     //Hamburger Animation
+//     hamburger.classList.toggle("toggle");
+// });
